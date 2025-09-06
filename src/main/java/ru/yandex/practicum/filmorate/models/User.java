1e18f34056ac8570bbf4a287fe507bdefbb2437e
Roman <roma.chekalin@gmail.com>
2025-09-06 23:50:39 +0300

    Sprint 12 - Fixes for review #1

--- src/main/java/ru/yandex/practicum/filmorate/models/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/User.java
@@ -4,9 +4,7 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 @Data
 public class User {
@@ -15,6 +13,5 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
-//    private Set<Long> friends = new HashSet<>();
     private Map<Long, Boolean> friends = new HashMap<>();
 }
